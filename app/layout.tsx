bc3beb4024b7c3f3e7a74607ce6185f54e7297ec
炳 <63220331+wcbing@users.noreply.github.com>
2023-05-22 20:21:23 +0800

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,56 @@
+import "@/styles/globals.css"
+
+import { SiteHeader } from "@/components/site-header"
+import { TailwindIndicator } from "@/components/tailwind-indicator"
+import { ThemeProvider } from "@/components/theme-provider"
+import { siteConfig } from "@/config/site"
+import { fontSans } from "@/lib/fonts"
+import { cn } from "@/lib/utils"
+import { Analytics } from "@vercel/analytics/react"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: {
+		default: siteConfig.name,
+		template: `%s - ${siteConfig.name}`,
+	},
+	description: siteConfig.description,
+	themeColor: [
+		{ media: "(prefers-color-scheme: light)", color: "white" },
+		{ media: "(prefers-color-scheme: dark)", color: "black" },
+	],
+	icons: {
+		icon: "/favicon.ico",
+		shortcut: "/favicon-16x16.png",
+		apple: "/apple-touch-icon.png",
+	},
+}
+
+interface RootLayoutProps {
+	children: React.ReactNode
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+	return (
+		<>
+			<html lang="en" suppressHydrationWarning className="h-full">
+				<head />
+				<body
+					className={cn(
+						"h-full bg-background font-sans antialiased",
+						fontSans.variable
+					)}
+				>
+					<ThemeProvider>
+						<div className="relative flex h-full flex-col">
+							<SiteHeader />
+							<div className="flex-1">{children}</div>
+						</div>
+						<TailwindIndicator />
+					</ThemeProvider>
+					<Analytics />
+				</body>
+			</html>
+		</>
+	)
+}
---
+++ app/subscription/[...slug]/page.tsx
@@ -0,0 +1,81 @@
+import { notFound } from "next/navigation"
+
+import FeedListGroup from "@/components/feed-list-group"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+	getFeedInfoList,
+	getFeedList,
+	getFeedListGroupedByYearAndMonth,
+	getFilters,
+} from "@/lib/notion"
+import { capitalize } from "@/lib/utils"
+
+export const revalidate = 3600
+
+export default async function SubscriptionPage({
+	params,
+}: {
+	params: {
+		slug: string[]
+	}
+}) {
+	if (params.slug.length !== 2) notFound()
+
+	const feedInfoList = await getFeedInfoList()
+	if (!feedInfoList) return null
+	const filters = await getFilters(feedInfoList)
+	if (!filters) return null
+
+	const feedListGroupedByYearAndMonth = getFeedListGroupedByYearAndMonth(
+		(await getFeedList(feedInfoList, params.slug[0], params.slug[1])) ?? []
+	)
+
+	return (
+		<>
+			<div className="container my-14 w-full max-w-5xl">
+				<div className="flex flex-col gap-4 md:flex-row">
+					{params.slug.map((slug, index) => {
+						return (
+							<Tabs defaultValue={slug} key={index}>
+								<TabsList>
+									{filters[index].map((filter) => (
+										<TabsTrigger
+											key={filter}
+											value={filter}
+											href={
+												index === 0
+													? `/subscription/${filter}/${params.slug[1]}`
+													: `/subscription/${params.slug[0]}/${filter}`
+											}
+										>
+											{capitalize(filter)}
+										</TabsTrigger>
+									))}
+								</TabsList>
+							</Tabs>
+						)
+					})}
+				</div>
+
+				<FeedListGroup
+					feedListGroupedByYearAndMonth={feedListGroupedByYearAndMonth}
+				/>
+			</div>
+		</>
+	)
+}
+
+export async function generateStaticParams() {
+	const filter = await getFilters()
+	if (!filter) return []
+
+	return [
+		...filter[0].flatMap((type) =>
+			filter[1].map((language) => ({
+				params: {
+					slug: [type, language],
+				},
+			}))
+		),
+	]
+}
